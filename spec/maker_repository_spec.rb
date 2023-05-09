b7f82a2b403ef23368df500cf50d43ba4215c2ba
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-05-09 13:58:20 +0100

    change name in maker_repository_spec

--- spec/maker_repository_spec.rb
+++ spec/maker_repository_spec.rb
@@ -1,29 +1,31 @@
 # file: spec/maker_repository_spec.rb
 
-def reset_makers_table
-  seed_sql = File.read('spec/chitter_seeds.sql')
-  connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_db_test' })
-  connection.exec(seed_sql)
-end
 
-describe MakerRepository do
-  before(:each) do 
-    reset_makers_table
+RSpec.describe MakerRepository do
+  def reset_makers_table
+    seed_sql = File.read('spec/chitter_seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_db_test' })
+    connection.exec(seed_sql)
   end
 
-# 1
-# Get all makers
+  describe MakerRepository do
+    before(:each) do 
+      reset_makers_table
+    end
 
-repo = MakerRepository.new
+  describe '#all' do
+    it 'returns all makers' do
 
-makers = repo.all
+    repo = MakerRepository.new
+    makers = repo.all
 
-makers.length # =>  '4'
-
-makers[0].id # =>  1
-makers[1].name # =>  'Ilaria'
-makers[2].email # =>  'chiara@fakemail.com'
-makers[3].username # =>  'barbara668'
+    expect(makers.length).to eq 4
+    expect(makers[0].id).to eq('1')
+    expect(makers[1].name).to eq('Ilaria')
+    expect(makers[2].email).to eq('chiara@fakemail.com')
+    expect(makers[3].username).to eq('barbara668')
+    end
+  end
 
 end
 

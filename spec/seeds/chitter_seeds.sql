6cb36bd8d83a258651d67c8de9adeed3aef19965
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-05-09 16:14:14 +0100

    correct seeds

--- spec/seeds/chitter_seeds.sql
+++ spec/seeds/chitter_seeds.sql
@@ -9,5 +9,5 @@ INSERT INTO makers (name, email, username, password) VALUES
 INSERT INTO peeps (title, content, time, maker_id) VALUES 
 ('Post 1', 'Hello, this is the first content.', '2023-01-08 04:05:06', '2'),
 ('Post 2', 'Hello, this is the second content.', '2023-02-24 04:05:06', '1'),
-('Post 3', 'Hello, this is the third content.' '2023-03-30 04:05:06', '3'),
+('Post 3', 'Hello, this is the third content.', '2023-03-30 04:05:06', '3'),
 ('Post 4', 'Hello, this is the fourth content.', '2023-04-10 04:05:06', '4');
